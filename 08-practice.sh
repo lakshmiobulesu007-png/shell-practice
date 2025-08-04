37ca1eb4235c806468059b3c2d7229bcba0e6168
lakshmiobulesu007-png <lakshmiobulesu007@gmail.com>
2025-08-04 21:52:40 +0530

    08-practice.sh

--- 08-practice.sh
+++ 08-practice.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-NUMBER1=$4
+NUMBER1=$1
 NUMBER2=$2
 
-SUM=$(($NUMBER4+$NUMBER2))
+SUM=$(($NUMBER1+$NUMBER2))
 
-echo "Total of '$NUMBER4+$NUMBER2' is : $SUM"
+echo "Total of $NUMBER1+$NUMBER2 is : $SUM"
