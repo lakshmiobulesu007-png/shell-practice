983fa54ddaa1ba2c2c948c803d6d2bc8fdde3acc
lakshmiobulesu007-png <lakshmiobulesu007@gmail.com>
2025-08-04 19:51:13 +0530

    02-practice

---
+++ 02-practice.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+
+echo "guru: : hi nani, how are you?"
+echo "nani: :hallo guru. i am good, how are you you doying?"
+echo "guru: :i am good nani. what going on?"
+echo "nani: :i am started lerning shell sriping guru"
