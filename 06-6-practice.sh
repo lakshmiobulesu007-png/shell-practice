3d89e1658b327cfc76faa0453b8a21311d071d91
lakshmiobulesu007-png <lakshmiobulesu007@gmail.com>
2025-08-04 21:21:56 +0530

    06-6-practice.sh

--- 06-6-practice.sh
+++ 06-6-practice.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-#indext start from 0 , size is 3
+#indext start from 1 , size is 3
 FRUITS=("APPLE" "KIWI" "ORANGE") #Array
 
-echo "first fruit is: ${FRUITS[0]}"
-echo "second fruit is: ${FRUITS[1]}"
-echo "thard fruit is: ${FRUITS[2]}"
+echo "first fruit is: ${FRUITS[1]}"
+echo "second fruit is: ${FRUITS[2]}"
+echo "thard fruit is: ${FRUITS[3]}"
 
 echo "total fruit are: ${FRUITS[@]}"
