099f48241bd832b1527159751b3207c8d6ed6d76
lakshmiobulesu007-png <lakshmiobulesu007@gmail.com>
2025-08-04 20:52:10 +0530

    05-practice.sh

---
+++ 05-practice.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+echo "plese enter your username::"
+read -s username
+echo "username enterd $username"
+echo "plese enter your password::"
+read -s password
+echo "enter password $password"
