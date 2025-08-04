fd8f9c07b1f7bd5fcaf8080b6b293c981032f166
lakshmiobulesu007-png <lakshmiobulesu007@gmail.com>
2025-08-04 20:38:05 +0530

    04-practice.sh

--- 02-practice.sh
+++ 02-practice.sh
@@ -3,6 +3,6 @@
 
 
 echo "guru: : hi nani, how are you?"
-echo "nani: :hallo guru. i am good, how are you you doying?"
-echo "guru: :i am good nani. what going on?"
-echo "nani: :i am started lerning shell sriping guru"
+echo "nani: : hallo guru. i am good, how are you you doying?"
+echo "guru: : i am good nani. what going on?"
+echo "nani: : i am started lerning shell sriping guru"
---
+++ 04-practice.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+person1=$1
+person2=$2
+
+
+echo "$person1: : hi $person2, how are you?"
+echo "$person2: : hallo $person1u. i am good, how are you you doying?"
+echo "$person1: : i am good $person2. what going on?"
+echo "$person2: : i am started lerning shell sriping $person1"
