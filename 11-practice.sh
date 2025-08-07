c68e2def2dd804a342159b0cad918c4923422454
lakshmiobulesu007-png <lakshmiobulesu007@gmail.com>
2025-08-07 21:23:08 +0530

    installation

---
+++ 11-practice.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+USERID=$(id -u)
+#echo "user ID is: $USERID"
+
+if [ $USERID -ne 0 ]
+then
+    echo "plese run this sript root priviligation"
+    exit 1
+fi
+
+
+dnf list installad git # jest checkong gether installed or not
+
+fi [ $? -ne 0 ]
+then
+    echo "git is not installed , going to install it"
+    dnf install git -y
+    fi[ $? -ne 0 ]
+    then
+        echo "git installation is not success..chick it"
+        exit 1
+    else
+        echo " git installation success"
+    fi
+else
+    echo "git is allrady installed, nothing to do"
+fi
+
+dnf list installed mysql
+
+fi [ $? -ne 0 ]
+then 
+    echo " mysql is not installed, going to install it"
+    dnf install mysql -y
+    fi [ $? -ne 0 ]
+    then
+        echo " mysql installation is not success, plece chit it"
+        exit 1
+    else
+        echo "mysql installation success"
+    fi
+else
+    echo "mysql allrady installed, nothing to do"
+fi
